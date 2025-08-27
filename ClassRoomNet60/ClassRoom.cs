f635807f682f3e474ba6730c0fc3f7b3c55981ee
Mikkel Barfod <mib010@edu.zealand.dk>
2025-08-27 11:42:13 +0200

    Add ClassRoom class

---
+++ ClassRoomNet60/ClassRoom.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoomNet60
+{
+    public class ClassRoom
+    {
+        public string className;
+        public List<Student> studentList;
+        public DateTime semesterStart;
+
+        public ClassRoom()
+        {
+
+        }
+    }
+}
