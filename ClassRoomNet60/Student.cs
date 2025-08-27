5cd2e577ffb002b38046c30a4d2f2b9f601cf3a8
Mikkel Barfod <mib010@edu.zealand.dk>
2025-08-27 11:35:15 +0200

    Add Student class

---
+++ ClassRoomNet60/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoomNet60
+{
+    public class Student
+    {
+        public string name { get; private set; }
+        public int birthmonth { get; private set; }
+        public int birthday { get; private set; }
+
+        public Student(string Name, int Bm, int Bd)
+        {
+            name = Name;
+            birthmonth = Bm;
+            birthday = Bd;
+        }
+    }
+}
