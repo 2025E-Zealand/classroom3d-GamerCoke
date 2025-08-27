d6c759250b51e8dfe5e11d8a14c9608e6b8f45b8
Mikkel Barfod <mib010@edu.zealand.dk>
2025-08-27 12:00:37 +0200

    Add object creationg to Program class

--- ClassRoomNet60/Program.cs
+++ ClassRoomNet60/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ClassRoomNet60;
+
+Console.WriteLine("Hello, World!\n");
+
+ClassRoom newClass = new ClassRoom();
+newClass.className = "3Q";
+newClass.semesterStart = new DateTime(2019-08-26);
+
+Student mikkel = new Student("Mikkel Barfod", 12, 15);
+Student malthe = new Student("Malthe Anders Bruhn", 9, 14);
+Student sara = new Student("Sara Iversen", 8, 9);
+
+newClass.studentList.Add(mikkel);
+newClass.studentList.Add(malthe);
+newClass.studentList.Add(sara);
